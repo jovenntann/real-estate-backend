e9cb652e66c83b21a33316a95095c68cde9d7948
Joven Tan <jovenntann@gmail.com>
2024-04-14 17:21:19 +0800

    refactor(sync_chats): Streamline chat sync process

--- domain/facebook/management/commands/sync_chats.py
+++ domain/facebook/management/commands/sync_chats.py
@@ -1,140 +1,122 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
-
 import logging
-logger = logging.getLogger(__name__)
 
-# Services
+# Models
+from domain.system.models.Company import Company
+from domain.facebook.models.Page import Page
+from domain.lead.models.Lead import Lead
+
+# Importing services and utilities
 from domain.system.services.company import get_company_by_id
 from domain.facebook.services.page import get_page_by_page_id
 from domain.lead.services.status import get_status_by_id
 from domain.lead.services.lead import create_lead, get_lead_by_facebook_id, update_lead_last_message_at
-from domain.facebook.services.chat import get_chat_by_message_id, create_chat
 from domain.lead.services.message import create_message, get_message_by_messenger_id
+from domain.facebook.utils.facebook import (
+    get_all_conversation, 
+    get_all_messages_by_conversation_id, 
+    get_message_by_message_id, 
+    get_user_profile_by_id
+)
 
-# Utilities
-from domain.facebook.utils.facebook import get_all_conversation, get_all_messages_by_conversation_id, get_message_by_message_id, get_user_profile_by_id
+# Data Class
+from domain.facebook.utils.facebook import ConversationData, MessageDetailData, MessageData
+
+
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     help = 'Create system sample data'
- 
+
     def handle(self, *args, **options):
         self.sync_chats()
 
     def sync_chats(self):
         company = get_company_by_id(id=1)
         page = get_page_by_page_id(page_id=113575558420278)
-        
+        self.process_conversations(page=page, company=company)
+
+    def process_conversations(self, page: Page, company: Company):
         conversations = get_all_conversation(access_token=page.access_token, page_id=page.page_id)
-        if conversations is not None:
-            for conversation in conversations.data[:500]:
-                logger.info(f"Conversation ID: {conversation.id}, Link: {conversation.link}, Updated Time: {conversation.updated_time}")
-                # TODO: if Conversation ID and Updated_Time already exist Skip (Might need to add new field for conversation_id -- tied to the Lead model)
-                self.process_messages_for_conversation(page, company, conversation)
-        else:
+        if not conversations:
             logger.error("No conversations found.")
+            return
 
-    def process_messages_for_conversation(self, page, company, conversation, next_url = None):
-        self.process_messages(page, company, conversation, next_url)
+        for conversation in conversations.data[:500]:
+            logger.info(f"Conversation ID: {conversation.id}, Link: {conversation.link}, Updated Time: {conversation.updated_time}")
+            self.process_conversation_messages(page=page, company=company, conversation=conversation)
 
-    def process_messages(self, page, company, conversation, next_url = None):
+    def process_conversation_messages(self, page: Page, company: Company, conversation: ConversationData, next_url=None):
         messages = get_all_messages_by_conversation_id(access_token=page.access_token, conversation_id=conversation.id, next_url=next_url)
         logger.info(messages)
 
         for message in messages.data:
-            message_detail = self.get_message_detail(page.access_token, message.id)
-            # If Chat Already exist let's skip thie Conversation (Only do this if all the messages are already migrated)
-            chat = get_chat_by_message_id(message_id=message_detail.data.id)
-            if chat:
-                logger.info(f"Chat already exists for message id: {message_detail.data.id}. Skipping..")
+
+            message_details = get_message_by_message_id(page.access_token, message.id)
+            if message_details is None:
+                logger.error(f"No message details found for message id: {message.id}. Skipping..")
                 return
-            self.process_message_detail(page, company, message.created_time, message, message_detail, conversation)
+            
+            message_record = get_message_by_messenger_id(messenger_id=message_details.data.id)
+            if message_record:
+                logger.info(f"Message already exists for messenger message id: {message_details.data.id}. Skipping..")
+                return
+            
+            # Calculated Variables
+            sender_type = 'page' if page.page_id == message_details.data.sender.id else 'lead'
+            user_id = message_details.data.sender.id if sender_type == 'lead' else message_details.data.recipient.data[0].id
+
+            lead = self.get_or_create_lead(page=page, company=company, user_id=user_id)
+            if not lead:
+                logger.info(f"No lead found for message id: {message_details.data.id}. Skipping..")
+                return
+
+            self.create_and_log_message(
+                page=page, 
+                lead=lead, 
+                conversation=conversation, 
+                message=message, 
+                message_details=message_details, 
+                sender_type=sender_type
+            )
 
         if messages.paging.next:
-            next_url = messages.paging.next
-            self.process_messages(page, company, conversation, next_url)
+            self.process_conversation_messages(page, company, conversation, messages.paging.next)
 
-    # PROCESS MESSAGE DETAILS
+    def get_or_create_lead(self, page: Lead, company: Company, user_id: int):
+        lead = get_lead_by_facebook_id(facebook_id=user_id)
+        if lead:
+            return lead
 
-    def get_or_create_lead_for_sender(self, company, page, message_detail):
-        lead = get_lead_by_facebook_id(facebook_id=message_detail.data.sender.id)
-        if lead is None:
-            status = get_status_by_id(id=1)
-            user_profile = get_user_profile_by_id(access_token=page.access_token, user_id=message_detail.data.sender.id)
-            lead = create_lead(
-                first_name=user_profile.data.first_name,
-                last_name=user_profile.data.last_name,
-                email=f"{user_profile.data.id}@facebook.com",
-                phone_number='',
-                company=company,
-                status=status,
-                facebook_id=user_profile.data.id,
-                facebook_profile_pic=user_profile.data.profile_pic if user_profile and user_profile.data else 'https://cdn.pixabay.com/photo/2013/07/13/10/44/man-157699_640.png'
-            )
-        return lead
-    
-    def get_or_create_lead_for_recipient(self, company, page, message_detail):
-        logger.info(message_detail)
-        lead = get_lead_by_facebook_id(facebook_id=message_detail.data.recipient.data[0].id)
-        if lead is None:
-            status = get_status_by_id(id=1)
-            user_profile = get_user_profile_by_id(access_token=page.access_token, user_id=message_detail.data.recipient.data[0].id)
-            lead = create_lead(
-                first_name=user_profile.data.first_name,
-                last_name=user_profile.data.last_name,
-                email=f"{user_profile.data.id}@facebook.com",
-                phone_number='',
-                company=company,
-                status=status,
-                facebook_id=user_profile.data.id,
-                facebook_profile_pic=user_profile.data.profile_pic if user_profile and user_profile.data else 'https://cdn.pixabay.com/photo/2013/07/13/10/44/man-157699_640.png'
-            )
-        return lead
+        user_profile = get_user_profile_by_id(access_token=page.access_token, user_id=user_id)
+        if not user_profile:
+            logger.info("Unable to get user profile. Skipping lead creation.")
+            return None
 
-    def get_or_create_chat(self, page, company, created_time, message_detail, conversation):
-        # Let's check if chat already exist based on Message ID
-        chat = get_chat_by_message_id(message_id=message_detail.data.id)
-        # If Not let's create the Chat
-        if chat is None:
-            if page.page_id == message_detail.data.sender.id:
-                sender = 'page'
-                lead = self.get_or_create_lead_for_recipient(company, page, message_detail)
-            else:
-                sender = 'lead'
-                lead = self.get_or_create_lead_for_sender(company, page, message_detail)
-            chat = create_chat(
-                message_id=message_detail.data.id,
-                sender=sender,
-                page=page,
-                lead=lead,
-                message=message_detail.data.message,
-                attachments=message_detail.data.attachments,
-                timestamp=created_time
-            )
-            # Let's also create record for Lead Message if not existing
-            message = get_message_by_messenger_id(messenger_id=message_detail.data.id)
-            if message is None:
-                create_message(
-                    page=page,
-                    lead=lead,
-                    source='messenger',
-                    sender=sender,
-                    message=message_detail.data.message,
-                    timestamp=created_time,
-                    messenger_id=message_detail.data.id,
-                    messenger_attachments=message_detail.data.attachments,
-                    is_read=False
-                )
-                # Update Lead last_message_at based on Conversation data (this will be repeated)
-                update_lead_last_message_at(lead=lead, last_message_at=conversation.updated_time)
-        return chat
-    
-    def get_message_detail(self, access_token, message_id):
-        return get_message_by_message_id(access_token, message_id)
+        status = get_status_by_id(id=1)
+        return create_lead(
+            first_name=user_profile.data.first_name,
+            last_name=user_profile.data.last_name,
+            email=f"{user_profile.data.id}@facebook.com",
+            phone_number='',
+            company=company,
+            status=status,
+            facebook_id=user_profile.data.id,
+            facebook_profile_pic=user_profile.data.profile_pic or 'https://cdn.pixabay.com/photo/2013/07/13/10/44/man-157699_640.png'
+        )
 
-    def process_message_detail(self, page, company, created_time, message, message_detail, conversation):
-        if message_detail is not None:
-            logger.info(f"Processing Message Details ID: {message_detail.data.id}")
-            chat = self.get_or_create_chat(page, company, created_time, message_detail, conversation)
-        else:
-            logger.error(f"No details found for message id: {message.id}")
+    def create_and_log_message(self, page: Page, lead: Lead, conversation: ConversationData, message: MessageData, message_details: MessageDetailData, sender_type: str):
+        create_message(
+            page=page,
+            lead=lead,
+            source='messenger',
+            sender=sender_type,
+            message=message_details.data.message,
+            timestamp=message.created_time,
+            messenger_id=message_details.data.id,
+            messenger_attachments=message_details.data.attachments,
+            is_read=False
+        )
+        update_lead_last_message_at(lead=lead, last_message_at=conversation.updated_time)
+        logger.info(f"Message logged: {message_details.data.message}")
