3faade52aa5e92462107c0c3c2dd22c2b04dfc62
Joven Tan <jovenntann@gmail.com>
2024-04-22 22:24:06 +0800

    feat(webhook): Add status and next action serializers

--- api/webhook/facebook/serializers.py
+++ api/webhook/facebook/serializers.py
@@ -4,9 +4,40 @@ from rest_framework import serializers
 from domain.lead.models.Message import Message
 from domain.lead.models.Lead import Lead
 from domain.facebook.models.Page import Page
+from domain.lead.models.Status import Status
+from domain.lead.models.NextAction import NextAction
+
+
+class ReadStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        ref_name = "agent.leads.messages.ReadStatusSerializer"
+        model = Status
+        fields = [
+            'id',
+            'status',
+            'color',
+            'description'
+        ]
+
+
+class ReadNextActionSerializer(serializers.ModelSerializer):
+    class Meta:
+        ref_name = "agent.leads.messages.ReadNextActionSerializer"
+        model = NextAction
+        fields = [
+            'id',
+            'action',
+            'color',
+            'description'
+        ]
 
 
 class LeadSerializer(serializers.ModelSerializer):
+
+    status = ReadStatusSerializer(read_only=True)
+    next_action = ReadNextActionSerializer(read_only=True)
+    last_message = serializers.SerializerMethodField()
+
     class Meta:
         ref_name = "webhook.facebook.LeadSerializer"
         model = Lead
@@ -18,7 +49,10 @@ class LeadSerializer(serializers.ModelSerializer):
             'phone_number',
             'company',
             'status',
-            'facebook_id'
+            'facebook_id',
+            'facebook_profile_pic',
+            'last_message_at',
+            'last_message'
         ]
 
 
